bf6c5fc7ab337f69f131708fd752ba07eaad5e7e
SaptakD625 <66028584+Saptak625@users.noreply.github.com>
2022-03-13 18:18:51 +0000

    Initial commit

---
+++ UIComponents.js
@@ -0,0 +1,465 @@
+class UIComponent {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.show = true;
+    }
+
+    draw() {}
+}
+
+class Button extends UIComponent {
+    constructor(x, y, sizeX, sizeY) {
+        super(x, y);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+    }
+
+    draw(fillColor, strokeColor, weight, roundedEdge) {
+        fill(fillColor);
+        stroke(strokeColor);
+        strokeWeight(weight);
+        rect(this.x, this.y, this.sizeX, this.sizeY, roundedEdge);
+    }
+
+    checkPressed(action = function() {}) {
+        if (mouseX >= this.x && mouseX <= this.x + this.sizeX && mouseY >= this.y && mouseY <= this.y + this.sizeY) {
+          action();
+          return true;
+        }
+        return false;
+    }
+    
+}
+
+class Navtab extends Button {
+    constructor(x, y, sizeX, sizeY, label, fontSize) {
+        super(x, y, sizeX, sizeY);
+        this.label = label;
+        this.fontSize = fontSize;
+        this.showUpdate = false;
+    }
+
+    draw(state, hover) {
+        if(hover) {
+            super.draw(16, 0, 2, 8);
+        } else if(state) {
+            super.draw(8, 0, 2, 8);
+        }
+        textAlign(CENTER, CENTER);
+        textSize(this.fontSize);
+        fill(157);
+        if(state){
+            fill(255);
+        }
+        noStroke();
+        text(this.label, this.x + (this.sizeX/2), this.y + (this.sizeY/2));
+        if(this.showUpdate) {
+            fill(204, 0, 0);
+            ellipse(this.x + this.sizeX - 10, this.y + 15, 10, 10);
+        }
+    }
+}
+
+class Navbar extends UIComponent {
+    constructor(x, y, sizeX, sizeY, tabs, activeTab) {
+        super(x, y);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.tabs = {};
+        var currentY = y;
+        for(let t of tabs){
+            this.tabs[t] = new Navtab(this.x, currentY, this.sizeX, this.sizeY, t, 18);
+            currentY += sizeY;
+        }
+        this.activeTab = activeTab;
+        this.showUpdate = false;
+    }
+
+    draw() {
+        fill(34);
+        stroke(34);
+        strokeWeight(2);
+        rect(this.x, this.y, this.sizeX, this.sizeY * Object.keys(this.tabs).length, 8)
+        for(let t in this.tabs) {
+            this.tabs[t].draw(t == this.activeTab, this.tabs[t].checkPressed());
+        }
+
+        this.tabs["Homeostasis"].showUpdate = this.showUpdate;
+    }
+
+    checkPressed() {
+      for(let t in this.tabs) {
+          if(this.tabs[t].checkPressed()) {
+              this.activeTab = t;
+              break;
+          }
+      }
+    }
+}
+
+class LevelBar extends UIComponent {
+    constructor(x, y, sizeX, sizeY, percentageComplete) {
+        super(x, y);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.percentageComplete = percentageComplete;
+        this.level = 1;
+    }
+
+    draw(target) {
+        if(this.percentageComplete > 1){
+            this.percentageComplete = 1;
+        }
+        stroke(0);
+        fill(34);
+        rect(this.x, this.y, this.sizeX, this.sizeY, 4);
+        fill(0, 128, 43);
+        rect(this.x, this.y, this.sizeX * this.percentageComplete, this.sizeY, 4);
+        textAlign(RIGHT, CENTER);
+        textSize(18);
+        noStroke();
+        fill(255);
+        var xDimension = this.x + (this.sizeX * this.percentageComplete)-10;
+        if(this.percentageComplete < 0.05){
+            xDimension += 40;
+        }
+        text(Math.round((this.percentageComplete * 100)) + "%", xDimension, this.y + (this.sizeY/2));
+        textAlign(CENTER, CENTER);
+        textStyle(BOLD);
+        fill(255);
+        text("Level " + this.level, this.x + (this.sizeX/2), this.y + (this.sizeY/2) - 40);
+        textStyle(NORMAL);
+        if(target > 0){
+            text(target + " more ATP", this.x + (this.sizeX/2), this.y + (this.sizeY/2) + 45); 
+        }
+    }
+
+    levelUp() {
+        this.level += 1;
+        this.percentageComplete = 0;
+    }
+}
+
+class Profile extends UIComponent {
+    constructor(x, y, diameter, organism) {
+        super(x, y);
+        this.diameter = diameter;
+        this.organism = organism;
+    }
+
+    draw(profileImage) {
+        cyanobacteriaImage.mask(profileMask);
+        image(cyanobacteriaImage, this.x, this.y, this.diameter, this.diameter);
+        noFill();
+        stroke(255);
+        strokeWeight(5);
+        ellipse(this.x + (this.diameter/2), this.y + (this.diameter/2), this.diameter, this.diameter);
+        noStroke();
+        fill(255);
+        textAlign(CENTER, CENTER);
+        textSize(16);
+        textStyle(BOLD);
+        text(this.organism, this.x + (this.diameter/2), this.y + this.diameter + 15);
+        textStyle(NORMAL);
+    }
+}
+
+class ScoreBox extends UIComponent {
+    constructor(x, y, sizeX, sizeY, score) {
+        super(x, y);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.score = score;
+    }
+
+    draw() {
+        fill(8);
+        rect(this.x, this.y, this.sizeX, this.sizeY, 15);
+        fill(255);
+        textSize(26);
+        noStroke();
+        textAlign(CENTER, CENTER);
+        text(this.score + " ATP", this.x + (this.sizeX/2), this.y + (this.sizeY/2));
+    }
+}
+
+class VerticalScrollBar extends UIComponent {
+    constructor(x, y, sizeX, sizeY, yDimension) {
+        super(x, y);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.yDimension = yDimension;
+        this.yPosition = this.y;
+    }
+
+    draw() {
+        fill(100);
+        stroke(100);
+        strokeWeight(2);
+        rect(this.x, this.y, this.sizeX, this.sizeY, this.sizeX/2);
+        fill(34);
+        noStroke();
+        rect(this.x, this.yPosition, this.sizeX, this.sizeY*(this.sizeY/this.yDimension), this.sizeX/2);
+    }
+
+    checkPressed() {
+        let b = new Button(this.x, this.yPosition, this.sizeX, this.sizeY*(this.sizeY/this.yDimension));
+        if(b.checkPressed()){
+            this.yPosition += movedY;
+        }
+        if(this.yPosition < this.y) {
+            this.yPosition = this.y;
+        }
+        // if(this.yPosition > (this.sizeY-(this.sizeY*(this.sizeY/this.yDimension)))){
+        //     this.yPosition = (this.sizeY-(this.sizeY*(this.sizeY/this.yDimension)));
+        // }
+    }
+}
+
+class QuantityBar extends UIComponent {
+    constructor(x, y, sizeX, sizeY, label, amount, maxAmount) {
+        super(x, y);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.label = label;
+        this.amount = amount;
+        this.maxAmount = maxAmount;
+    }
+
+    draw(c, barColor = 34) {
+        if(this.show) {
+            c.stroke(0);
+            c.fill(barColor);
+            c.rect(this.x, this.y, this.sizeX, this.sizeY, this.sizeY/2);
+            c.fill(0, 128, 43);
+            c.noStroke();
+            c.rect(this.x, this.y, this.sizeX * (this.amount / this.maxAmount), this.sizeY, this.sizeY/2);
+            c.noStroke();
+            c.fill(255);
+            c.textSize(18);
+            c.textAlign(CENTER, TOP);
+            c.text(this.amount + " / " + this.maxAmount, this.x + (this.sizeX/2), this.y + this.sizeY + 20); 
+        }
+    }
+
+    increment(number) {
+        this.amount += number;
+        if(this.amount > this.maxAmount) {
+            this.amount = this.maxAmount;
+        }
+    }
+
+    changeCoords(newX, newY) {
+        this.x = newX;
+        this.y = newY;
+    }
+}
+
+class ReactionUI extends QuantityBar {
+    constructor(x, y, sizeX, sizeY, reactionLabel, reactantLabel, productLabel, amount, maxAmount, increment, disabled = function() {false}, reactantImage = null, productImage = null) {
+        super(x, y, sizeX, sizeY, reactantLabel, amount, maxAmount);
+        this.reactionLabel = reactionLabel;
+        this.productLabel = productLabel;
+        this.increment = increment;
+        this.button = new Button(this.x, this.y, this.sizeX, this.sizeY);
+        this.reactantImage = reactantImage;
+        this.productImage = productImage;
+        this.disabled = disabled;
+    }
+  
+    draw(c) {
+        if(this.show) {
+            if(!this.disabled()){
+                super.draw(c); 
+            } else {
+                super.draw(c, 100);
+            }
+            c.stroke(0);
+            c.fill(255);
+            c.ellipse(this.x + this.sizeY/2, this.y + this.sizeY/2, this.sizeY + 20, this.sizeY + 20);
+            c.image(this.reactantImage, this.x, this.y, 60, 60);
+            if(this.productLabel !== null){
+                c.ellipse(this.x + this.sizeX - this.sizeY/2, this.y + this.sizeY/2, this.sizeY + 20, this.sizeY + 20);
+                c.image(this.productImage, this.x + this.sizeX - this.sizeY, this.y, 60, 60);
+            }
+            c.noStroke();
+            c.fill(255);
+            c.textSize(18);
+            c.textAlign(CENTER, CENTER);
+            c.text(this.label, this.x + this.sizeY/2, this.y + this.sizeY + 20);
+            if(this.productLabel !== null){
+                c.text(this.productLabel, this.x + this.sizeX - this.sizeY/2, this.y + this.sizeY + 20);
+            }
+            c.textSize(20);
+            c.text(this.reactionLabel, this.x + (this.sizeX/2), this.y - 15);
+        }
+    }
+
+    changeCoords(newX, newY, newButtonX, newButtonY) {
+        super.changeCoords(newX, newY);
+        this.button = new Button(newButtonX, newButtonY, this.sizeX, this.sizeY);
+    }
+
+    checkPressed(action = function() {}) {
+        if(this.show && !this.disabled()){
+            if(this.button.checkPressed() && mouseIsPressed === true) {
+                this.amount += this.increment;
+            }else {
+              this.amount = 0;
+            }
+            if(this.amount >= 100) {
+                this.amount = 0;
+                action();
+            } 
+        } 
+    }
+}
+
+class ProductUI extends QuantityBar {
+    constructor(x, y, sizeX, sizeY, label, amount, maxAmount, reactantImage) {
+        super(x, y, sizeX, sizeY, label, amount, maxAmount);
+        this.reactantImage = reactantImage;
+    }
+  
+    draw(c) {
+        if(this.show) {
+            super.draw(c);
+            c.stroke(0);
+            c.fill(255);
+            c.ellipse(this.x + this.sizeY/2, this.y + this.sizeY/2, this.sizeY + 20, this.sizeY + 20);
+            c.image(this.reactantImage, this.x, this.y, 60, 60);
+            c.noStroke();
+            c.fill(255);
+            c.textSize(18);
+            c.textAlign(CENTER, CENTER);
+            c.text(this.label, this.x + this.sizeY/2, this.y + this.sizeY + 20);
+        }
+    }
+
+    startRefill(action, numOfSeconds){
+        setInterval(action, numOfSeconds * 1000);
+    }
+}
+
+class CoenzymeUI extends QuantityBar {
+    draw(c) {
+        if(this.show) {
+            super.draw(c);
+            c.stroke(0);
+            c.fill(255);
+            c.ellipse(this.x + this.sizeY/2, this.y + this.sizeY/2, this.sizeY + 20, this.sizeY + 20);
+            c.noStroke();
+            c.fill(0);
+            c.textSize(22);
+            c.textAlign(CENTER, CENTER);
+            c.text(this.label, this.x + this.sizeY/2, this.y + this.sizeY/2);
+        }
+    }
+}
+
+class QuantityBarColumn extends UIComponent {
+    constructor(x, y, sizeX, sizeY, quantityBars) {
+        super(x, y);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.quantityBars = quantityBars;
+        this.graphics = createGraphics(this.sizeX, this.sizeY);
+    }
+
+    draw() {
+        this.graphics.clear();
+        var yCoord = 50;
+        var yCoordButton = this.y + yCoord;
+        for(let q of this.quantityBars) {
+            if(!q.show){
+                continue;
+            }
+            q.changeCoords(50, yCoord, this.x, yCoordButton);
+            q.draw(this.graphics);
+            yCoord += 150;
+            yCoordButton += 150;
+        }
+        image(this.graphics, this.x, this.y);
+    }
+}
+
+class HomeostasisMessage extends UIComponent {
+    constructor(x, y, sizeX, sizeY, message, actionMessage, action) {
+        super(x, y);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.message = message;
+        this.actionMessage = actionMessage;
+        this.action = action;
+        this.button = new Button(this.x + this.sizeX - 250, this.y + 20, 230, this.sizeY-40);
+        this.accepted = false;
+    }
+
+    draw() {
+        if(this.show){
+            noStroke();
+            fill(34);
+            rect(this.x, this.y, this.sizeX, this.sizeY, 6);
+            fill(8);
+            rect(this.x + this.sizeX - 250, this.y + 20, 230, this.sizeY-40, 6);
+            fill(255);
+            textAlign(LEFT, CENTER);
+            textSize(24);
+            text(this.message, this.x + 20, this.y + (this.sizeY/2));
+            textAlign(CENTER, CENTER);
+            textSize(18);
+            text(this.actionMessage, this.x + this.sizeX - 250 + (230/2), this.y + 20 + ((this.sizeY - 40)/2));
+        }
+    }
+
+    checkPressed() {
+        if(this.show && this.button.checkPressed()) {
+            this.show = false;
+            this.accepted = true;
+            this.action();
+        }
+    }
+
+    changeCoords(newX, newY) {
+        this.x = newX;
+        this.y = newY;
+        this.button = new Button(this.x + this.sizeX - 250, this.y + 20, 230, this.sizeY-40);
+    }
+}
+
+class MessageColumn extends UIComponent {
+    constructor(x, y, sizeX, sizeY, messages) {
+        super(x, y);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.messages = messages;
+    }
+
+    draw() {
+        var yCoord = this.y;
+        for(let m of this.messages) {
+            if(!m.show){
+                continue;
+            }
+            m.changeCoords(this.x, yCoord);
+            m.draw();
+            yCoord += 150;
+        }
+        if(yCoord === this.y) { //No Messages
+            textAlign(CENTER, CENTER);
+            fill(255);
+            textSize(32);
+            text("No Issues with Homeostasis!", this.x + (this.sizeX/2), this.y + 50);
+        }
+    }
+
+    checkForMessages() {
+        for(let m of this.messages) {
+            if(m.show) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ script.js
@@ -0,0 +1,236 @@
+//Images
+let backgroundImage;
+let cyanobacteriaImage;
+let profileMask;
+
+let glucoseImage;
+let pyruvateImage;
+let fructoseImage;
+let sucroseImage;
+let nadhImage;
+
+//UI Components
+let canvas;
+
+let navbar;
+let levelBar;
+let profile;
+let scoreBox;
+
+let reactions;
+let sucroseHydrolysis;
+let glycolysis;
+let fructolysis
+let krebs;
+let etc;
+
+let inventory;
+let glucose;
+let fructose;
+let pyruvate;
+
+let nadh;
+let fadh;
+
+let fructoseMessage;
+let sucroseMessage;
+let messageColumn;
+
+let levelTargets = [0, 500, 1750, 5000];
+let messageToShow;
+
+let scrollBar;
+
+function preload() {
+    backgroundImage = loadImage("assets/background.png");
+    cyanobacteriaImage = loadImage("assets/ecoli.jpg");
+    profileMask = loadImage("assets/profileMask.png");
+
+    glucoseImage = loadImage("assets/glucose.png");
+    pyruvateImage = loadImage("assets/pyruvate.svg");
+    fructoseImage = loadImage("assets/fructose.png");
+    sucroseImage = loadImage("assets/sucrose.png");
+
+    nadhImage = loadImage("assets/nadh.png");
+}
+
+function setup() {
+    canvas = createCanvas(displayWidth, displayHeight);
+    navbar = new Navbar(60 + (displayWidth/20), 250, 150, 60, ["Reactions", "Inventory", "Coenzymes", "Homeostasis", "Pathways"], "Reactions");
+    levelBar = new LevelBar((displayWidth - 800)/2, 50, 800, 60, 0);
+    profile = new Profile(90 + (displayWidth/20), 30, 100, "E. Coli");
+    scoreBox = new ScoreBox(displayWidth - 300, 50, 200, 60, 0);
+
+
+    glycolysis = new ReactionUI((displayWidth - 800)/2, 200, 800, 60, "Glycolysis", "Glucose", "Pyruvate + NADH", 0, 100, 2, function () {
+      return glucose.amount < 1;
+    }, reactantImage = glucoseImage, productImage = pyruvateImage);
+    krebs = new ReactionUI((displayWidth - 800)/2, 350, 800, 60, "Kreb's Cycle", "Pyruvate", "NADH + FADH2", 0, 100, 2, function () {
+      return pyruvate.amount < 2;
+    }, reactantImage = pyruvateImage, productImage = nadhImage);
+    etc = new ReactionUI((displayWidth - 800)/2, 500, 800, 60, "Electron Transport Chain", "NADH + FADH2", null, 0, 100, 2, function () {
+      return nadh.amount < 10 || fadh.amount < 2;
+    }, reactantImage = nadhImage);
+    fructolysis = new ReactionUI((displayWidth - 800)/2, 650, 800, 60, "Fructolysis", "Fructose", "Pyruvate + NADH", 0, 100, 4, function () {
+      return fructose.amount < 1;
+    }, reactantImage = fructoseImage, productImage = pyruvateImage);
+    fructolysis.show = false;
+    sucroseHydrolysis = new ReactionUI((displayWidth - 800)/2, 650, 800, 60, "Sucrose Hydrolysis", "Sucrose", "Glucose + Fructose", 0, 100, 1, function () {
+      return sucrose.amount < 1;
+    }, reactantImage = sucroseImage, productImage = glucoseImage);
+    sucroseHydrolysis.show = false;
+    reactions = new QuantityBarColumn((displayWidth - 900)/2, 200, 900, 700, [sucroseHydrolysis, glycolysis, fructolysis, krebs, etc]);
+
+    glucose = new ProductUI((displayWidth - 800)/2, 200, 800, 60, "Glucose", 10, 500, glucoseImage);
+    glucose.startRefill(function () {
+      glucose.increment(1);
+    }, 12);
+    pyruvate = new ProductUI((displayWidth - 800)/2, 350, 800, 60, "Pyruvate", 0, 500, pyruvateImage);
+    fructose = new ProductUI((displayWidth - 800)/2, 200, 800, 60, "Fructose", 10, 500, fructoseImage);
+    fructose.show = false;
+    sucrose = new ProductUI((displayWidth - 800)/2, 200, 800, 60, "Sucrose", 10, 500, sucroseImage);
+    sucrose.show = false;
+    inventory = new QuantityBarColumn((displayWidth - 900)/2, 200, 900, 700, [glucose, fructose, sucrose, pyruvate]);
+  
+    nadh = new CoenzymeUI((displayWidth - 800)/2, 200, 800, 60, "NADH", 0, 500);
+    fadh = new CoenzymeUI((displayWidth - 800)/2, 350, 800, 60, "FADH2", 0, 500);
+    coenzymes = new QuantityBarColumn((displayWidth - 900)/2, 200, 900, 700, [nadh, fadh]);
+    // localStorage.setItem("lastname", "Smith");
+
+    fructoseMessage = new HomeostasisMessage((displayWidth - 800)/2, 200, 800, 100, "Fructose Sugar found!", "Unlock Fructose Pathway", function () {
+      fructose.show = true;
+      fructolysis.show = true;
+      fructose.startRefill(function () {
+        fructose.increment(1);
+      }, 30);
+    });
+    fructoseMessage.show = false;
+
+    sucroseMessage = new HomeostasisMessage((displayWidth - 800)/2, 200, 800, 100, "Sucrose Sugar found!", "Unlock Sucrose Pathway", function () {
+      sucrose.show = true;
+      sucroseHydrolysis.show = true;
+      sucrose.startRefill(function () {
+        sucrose.increment(1);
+      }, 60);
+    });
+    sucroseMessage.show = false;
+
+    messageColumn = new MessageColumn((displayWidth - 800)/2, 200, 800, 700, [fructoseMessage, sucroseMessage]);
+
+    scrollBar = new VerticalScrollBar(600, 300, 20, 600, 800);
+}
+
+function draw() {
+    background(backgroundImage);
+
+  
+    //UPDATE BEFORE DRAW
+    //Held mouse interactions
+    glycolysis.checkPressed(function () {
+        glucose.increment(-1);
+        pyruvate.increment(2);
+        nadh.increment(2);
+        incrementATP(2);
+    }); 
+    krebs.checkPressed(function () {
+        pyruvate.increment(-2);
+        nadh.increment(8);
+        fadh.increment(2);
+        incrementATP(2);
+    }); 
+    etc.checkPressed(function () {
+        nadh.increment(-10);
+        fadh.increment(-2);
+        incrementATP(32);
+    });
+    fructolysis.checkPressed(function () {
+        fructose.increment(-1);
+        pyruvate.increment(2);
+        nadh.increment(2);
+        incrementATP(2);
+    }); 
+    sucroseHydrolysis.checkPressed(function () {
+        sucrose.increment(-1);
+        glucose.increment(1);
+        fructose.increment(1);
+    });
+
+    //Level Dynamics
+    var previousTarget = 0;
+    for (let i = 0; i < levelBar.level; i++) {
+        previousTarget += levelTargets[i];
+    }
+    var target = previousTarget + levelTargets[levelBar.level];
+    while(scoreBox.score >= target) {
+        if(levelBar.level < (levelTargets.length-1)){
+          levelBar.levelUp();
+          previousTarget = 0;
+          for (let i = 0; i < levelBar.level; i++) {
+              previousTarget += levelTargets[i];
+          }
+          target = previousTarget + levelTargets[levelBar.level];
+          navbar.showUpdate = true;
+          messageColumn.messages[levelBar.level - 2].show = true;
+        }else{
+          break;
+        }
+    }
+  
+    //Level Bar Calculation
+    // console.log("a"+previousTarget);
+    // console.log("b"+target);
+    levelBar.percentageComplete = (scoreBox.score - previousTarget) / levelTargets[levelBar.level]; 
+  
+    //Draw UI Components
+    profile.draw(cyanobacteriaImage);
+    navbar.draw();
+    levelBar.draw(target - scoreBox.score);
+    scoreBox.draw();
+    scrollBar.draw();
+  
+    //Notification
+    navbar.showUpdate = messageColumn.checkForMessages();
+  
+    switch (navbar.activeTab){
+        case "Reactions":
+            reactions.draw(); //Aligns Quantity Bars
+            // glycolysis.draw();
+            // krebs.draw();
+            // etc.draw();
+            // fructolysis.draw();
+            // sucroseHydrolysis.draw();
+            break;
+
+        case "Inventory":
+            inventory.draw(); //Aligns Quantity Bars
+            // glucose.draw(glucoseImage);
+            // pyruvate.draw(pyruvateImage);
+            // fructose.draw(fructoseImage);
+            // sucrose.draw(sucroseImage);
+            break;
+        
+        case "Coenzymes":
+            coenzymes.draw();
+            // nadh.draw();
+            // fadh.draw();
+            break;
+
+        case "Homeostasis":
+            messageColumn.draw();
+            break;
+    }
+}
+
+function mouseClicked() {
+    navbar.checkPressed();
+    fructoseMessage.checkPressed();
+    sucroseMessage.checkPressed();
+}
+
+function mouseDragged() {
+    scrollBar.checkPressed();
+}
+
+function incrementATP(number) {
+    scoreBox.score += number;
+}
